15a850d22128c3a63fa8f7a30c2cfc48758ab691
beyond88 <muhin.cse.diu@gmail.com>
2023-05-06 21:44:56 +0600

    Added plugin main file and readme txt

---
+++ give-tipping.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Give - Tipping Plugin
+ * Plugin URI: https://github.com/beyond88/give-tipping
+ * Description: The most robust, flexible, and intuitive way to accept donations on WordPress.
+ * Author: Mohiuddin Abdul Kader
+ * Author URI: https://github.com/beyond88/give-tipping/
+ * Version: 1.0.0
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * Text Domain: give-tipping
+ * Domain Path: /languages
+ *
+ */
